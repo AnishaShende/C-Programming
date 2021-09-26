b4f13a6b51b68c2a0f9ca2b34b13ec694d254eb7
AnishaShende <91362696+AnishaShende@users.noreply.github.com>
2021-09-26 20:57:01 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void 
+main () 
+{
+  
+ 
+int number;
+  
+ 
+printf ("Enter a number : ");
+  
+ 
+scanf ("%d", &number);
+  
+ 
+printf ("The number you enter was %d", number);
+
+ 
+}
