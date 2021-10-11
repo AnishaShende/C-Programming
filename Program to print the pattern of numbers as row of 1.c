efb26d99c6603eb993064dc1b8d063d66b1e8cc2
AnishaShende <91362696+AnishaShende@users.noreply.github.com>
2021-10-11 09:44:41 +0530

    Create Program to print the pattern of numbers as row of 1.c

---
+++ Program to print the pattern of numbers as row of 1.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, i, j;
+    printf("Enter a number : ");
+    scanf("%d", &a);
+    for (i = 1; i <= a; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
