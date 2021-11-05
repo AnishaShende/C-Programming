545c36311c0367ca28176a41a08ff90753c68ed0
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2021-11-05 22:15:26 +0530

    Create Function to display good morning,ga,gn.c

---
+++ Function to display good morning,ga,gn.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void gm();
+void ga();
+void gn();
+int main()
+{
+    printf("Hello World\n");
+    gm();
+    ga();
+    gn();
+    return 0;
+}
+void gm() {
+    printf("Good Morning !\n");
+}
+void ga() {
+    printf("Good Afternoon !\n");
+}
+void gn() {
+    printf("Good Night !\n");
+}
