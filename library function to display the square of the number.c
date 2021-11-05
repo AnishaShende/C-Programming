c55006d44e206409122fb2a68926177445c37746
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2021-11-05 23:03:49 +0530

    Create library function to display the square of the number.c

---
+++ library function to display the square of the number.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a;
+    printf("Enter a number : ");
+    scanf("%d", &a);
+    printf("Area of square is %.2f", pow(a,2));
+    return 0;
+}
