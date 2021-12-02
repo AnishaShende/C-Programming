b5676a892684b07efb75563327bbc03791740571
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2021-12-02 09:40:12 +0530

    Create searching array.c

---
+++ searching array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void
+main ()
+{
+  int a[10], n;
+  int i, search;
+
+  printf ("Enter number of element : ");
+  scanf ("%d", &n);
+  printf ("Enter elements of an array : \n");
+  for (i = 0; i < n; i++)
+    {
+      scanf ("%d", &a[i]);
+
+    }
+  printf ("Enter number you want to search : ");
+  scanf ("%d", &search);
+  for (i = 0; i < n; i++)
+    {
+      if (search == a[i])
+	{
+	  printf ("%d element is present at %d location.\n", search, i);
+	  break;
+	}
+    }
+  if (i == n)
+    printf ("%d element is not present in array!", search);
+}
