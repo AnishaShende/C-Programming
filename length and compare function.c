25f595f87aaf7345e583800308f2b9b24ae780cc
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2021-12-02 14:59:29 +0530

    Create length and compare function.c

---
+++ length and compare function.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+void
+main ()
+{
+char str[20];
+char s1[50], s2[50];
+int j = strlen("HelloWorld");
+int i=strcmp("Hello ", "World");
+printf("%d %d",j,i);
+}
