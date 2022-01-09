cb3f75f2d6ebeb76179891ed60a91db346888327
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2022-01-09 19:07:25 +0530

    Create conditionals and loop error.c

---
+++ conditionals and loop error.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+  	// Complete the code.
+      /*for(int i = a; i <= 9; i++){
+          switch(i){
+              case 1:
+              printf("one\n");
+              break;
+              case 2:
+              printf("two\n");
+              break;
+              case 3:
+              printf("three\n");
+              break;
+              case 4:
+              printf("four\n");
+              break;
+              case 5:
+              printf("five\n");
+              break;
+              case 6:
+              printf("six\n");
+              break;
+              case 7:
+              printf("seven\n");
+              break;
+              case 8:
+              printf("eight\n");
+              break;
+              default:
+              printf("nine\n");
+          }*/
+          switch(a){
+              case 1:
+              printf("one\n");
+              break;
+              case 2:
+              printf("two\n");
+              break;
+              case 3:
+              printf("three\n");
+              break;
+              case 4:
+              printf("four\n");
+              break;
+              case 5:
+              printf("five\n");
+              break;
+              case 6:
+              printf("six\n");
+              break;
+              case 7:
+              printf("seven\n");
+              break;
+              case 8:
+              printf("eight\n");
+              break;
+              default:
+              printf("nine\n");
+          }
+      /*for(int i = b; i <= 9; i++){
+          switch(i){
+              case 1:
+              printf("one\n");
+              break;
+              case 2:
+              printf("two\n");
+              break;
+              case 3:
+              printf("three\n");
+              break;
+              case 4:
+              printf("four\n");
+              break;
+              case 5:
+              printf("five\n");
+              break;
+              case 6:
+              printf("six\n");
+              break;
+              case 7:
+              printf("seven\n");
+              break;
+              case 8:
+              printf("eight\n");
+              break;
+              default:
+              printf("nine\n");
+          }
+      }*/
+          switch(b){
+              case 1:
+              printf("one\n");
+              break;
+              case 2:
+              printf("two\n");
+              break;
+              case 3:
+              printf("three\n");
+              break;
+              case 4:
+              printf("four\n");
+              break;
+              case 5:
+              printf("five\n");
+              break;
+              case 6:
+              printf("six\n");
+              break;
+              case 7:
+              printf("seven\n");
+              break;
+              case 8:
+              printf("eight\n");
+              break;
+              default:
+              printf("nine\n");
+          }
+      if (a%2==0) {
+          printf("even\n");
+          }
+          else{
+              printf("odd\n");
+          }
+      if (b%2==0) {
+          printf("even\n");
+          }
+          else{
+              printf("odd\n");
+          }
+      /*for(int i = a; i <= 9; i++){
+          if (i%2==0) {
+          printf("even\n");
+          }
+          else{
+              printf("odd\n");
+          }
+      }
+      for(int i = b; i <= 9; i++){
+          if (i%2==0) {
+          printf("even\n");
+          }
+          else{
+              printf("odd\n");
+          }
+      }*/
+    return 0;
+}
