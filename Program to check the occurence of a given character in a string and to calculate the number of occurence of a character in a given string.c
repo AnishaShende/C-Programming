17f3370c781b3fd673a77524ca47181ab56dfe30
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2022-01-28 16:18:30 +0530

    Update Program to check the occurence of a given character in a string and to calculate the number of occurence of a character in a given string.c

--- Program to check the occurence of a given character in a string and to calculate the number of occurence of a character in a given string.c
+++ Program to check the occurence of a given character in a string and to calculate the number of occurence of a character in a given string.c
@@ -3,20 +3,21 @@
 int main()
 {
     char str[] = "anishaanisha";
+    char ch = 'z';
     int count = 0;
     int i = 0;
     while(str[i] != '\0'){
-        if(str[i] == 'a'){
+        if(str[i] == ch){
             count++;
         }
         i++;
     }
     if(count >= 1){
-        printf("a is present in the given string.\n");
-        printf("a occured %d times in a given string.", count);
+        printf("%c is present in the given string.\n", ch);
+        printf("%c occured %d times in a given string.", ch, count);
     }
     else{
-        printf("a is not present in the given string.");
+        printf("%c is not present in the given string.", ch);
     }
     return 0;
 }
